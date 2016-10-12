723c0bdee5f86233eb0b44570288870ae1a158c3
Ho Wai Yung <howaiyung1988@gmail.com>
2016-10-11 23:03:12 -0600

    Parsed scraping from website, testing parsing

--- src/HTMLParserMin.java
+++ src/HTMLParserMin.java
@@ -1,3 +1,4 @@
+import java.awt.List;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -7,12 +8,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.jsoup.Jsoup;
 import org.jsoup.parser.*;
+import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
 
+//https://jsoup.org/cookbook/input/load-document-from-url
 
 
 	
@@ -23,8 +26,43 @@ public class HTMLParserMin {
 		{
 			Document doc = Jsoup.connect("http://ca.indeed.com/jobs?q=developer&l=Calgary%2C+AB&sort=date").get();
 			
+			Elements scriptTags = doc.getElementsByTag("script");
 			
-			
+			for (Element tag : scriptTags){   
+				for (DataNode node : tag.dataNodes()){
+						//String[] temp = node.toString().split(" ");
+						String[] temp = node.toString().split(" ");
+						String temp2 = String.join(" ", temp);
+						temp2 = temp2.replaceAll("\\r", "");
+						
+						//Look for Window and Jobmap
+						System.out.println(temp2 + "\n");
+						
+						/*for(String part : temp)
+						{
+							if((part == "") || (part == null) || (part.isEmpty()))
+							{
+								
+							}
+							else
+							{
+								
+							}
+						}*/
+						//System.out.println(node.getWholeData());
+		           }        
+			}
+
+		    /*for (Element tag : scriptTags){                
+		           for (DataNode node : tag.dataNodes()) {
+		               System.out.println(node.getWholeData());
+		           }        
+		     }*/
+
+		    
+			/*for (Element t : script){
+				System.out.println(t.data());
+			}*/
 			
 			//Elements e = doc.getAllElements();
 			//for (Element t : e){
@@ -32,7 +70,7 @@ public class HTMLParserMin {
 				//System.out.println(t.data());
 			//}
 			
-			System.out.println(doc.select("script"));
+			//System.out.println(doc.select("script"));
 		}
 		catch (IOException e) 
 		{
